1fff6b460696c225ab80b86add8c92515c393aab
xh <xh@BDQN>
2017-06-22 16:23:03 +0800

    20170622修改

---
+++ src/cn/test/pojo/Book.java
@@ -0,0 +1,15 @@
+package cn.test.pojo;
+
+public class Book {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
